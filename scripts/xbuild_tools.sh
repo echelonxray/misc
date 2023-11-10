7089aa45b2491341027a54b9fd3f5034badbf908
Michael T. Kloos <michael@michaelkloos.com>
2023-11-10 07:01:25 -0500

    Update build script

--- scripts/xbuild_tools.sh
+++ scripts/xbuild_tools.sh
@@ -2,19 +2,23 @@
 
 # Start: Configuration
 
-MARCH=rv64ia
-MABI=lp64
+MARCH=rv32ia
+MABI=ilp32
 
-PREFIX_DIR="/home/quantum/devel/prefixes/try_musl_1"
+SYSROOT="/home/quantum/devel/prefixes/try_musl_0"
 
 BINUTILS_SOURCE_DIR="/home/quantum/devel/git/binutils-gdb"
 GCC_SOURCE_DIR="/home/quantum/devel/git/gcc"
+MPC_SOURCE_DIR="/home/quantum/devel/mpc/mpc"
+MPFR_SOURCE_DIR="/home/quantum/devel/mpfr/mpfr"
+GMP_SOURCE_DIR="/home/quantum/devel/gmp/gmp"
 LINUX_SOURCE_DIR="/home/quantum/devel/git/linux"
-MUSL_SOURCE_DIR="/home/quantum/devel/git/musl"
+LIBC_SOURCE_DIR="/home/quantum/devel/git/musl"
 
-TGT1=riscv64-ex1-elf
-TGT2=riscv64-ex2-linux-musl
-TGT3=riscv64-ex3-linux-musl
+TGT1=riscv32-rv32ia1-elf
+#TGT1=riscv32-rv32ia1-linux-musl
+TGT2=riscv32-rv32ia2-linux-musl
+TGT3=riscv32-rv32ia3-linux-musl
 
 HOST=x86_64-pc-linux-gnu
 
@@ -34,17 +38,17 @@ HOST=x86_64-pc-linux-gnu
 
 renice -n 10 $$
 
-export PATH=$PREFIX_DIR/bin:$PREFIX_DIR/usr/bin:$PREFIX_DIR/tools/bin:$PREFIX_DIR/tools/usr/bin:$PATH
+export PATH=$SYSROOT/bin:$SYSROOT/usr/bin:$SYSROOT/tools/bin:$SYSROOT/tools/usr/bin:$PATH
 
-rm -rf "$PREFIX_DIR"/*
-rm -rf "$PREFIX_DIR"/.*
+rm -rf "$SYSROOT"/*
+rm -rf "$SYSROOT"/.*
 
 # Binutils Stage 1
 cd "$BINUTILS_SOURCE_DIR"
 rm -rf ./build1
 mkdir ./build1
 cd ./build1
-./../configure --target=$TGT1 --host=$HOST --prefix="$PREFIX_DIR/tools/usr" --with-sysroot="$PREFIX_DIR/tools" --disable-nls --disable-werror
+./../configure --target=$TGT1 --host=$HOST --prefix="$SYSROOT/tools/usr" --with-sysroot="$SYSROOT/tools" --disable-nls --disable-werror
 make -j$(nproc) all
 CODE=$?
 if [ $CODE -ne 0 ]; then
@@ -58,7 +62,7 @@ cd "$GCC_SOURCE_DIR"
 rm -rf ./build1
 mkdir ./build1
 cd ./build1
-./../configure --target=$TGT1 --host=$HOST --prefix="$PREFIX_DIR/tools/usr" --with-sysroot="$PREFIX_DIR/tools" --with-newlib --without-headers --enable-initfini-array --disable-nls --disable-multilib --disable-decimal-float --disable-threads --disable-libatomic --disable-libgomp --disable-libquadmath --disable-libssp --disable-libvtv --disable-libstdcxx --enable-languages=c,c++ --with-arch=$MARCH --with-abi=$MABI
+./../configure --target=$TGT1 --host=$HOST --prefix="$SYSROOT/tools/usr" --with-sysroot="$SYSROOT/tools" --with-mpc="$MPC_SOURCE_DIR" --with-mpfr="$MPFR_SOURCE_DIR" --with-gmp="$GMP_SOURCE_DIR" --with-newlib --without-headers --enable-initfini-array --disable-nls --disable-multilib --disable-decimal-float --disable-threads --disable-libatomic --disable-libgomp --disable-libquadmath --disable-libssp --disable-libvtv --disable-libstdcxx --enable-languages=c,c++ --with-arch=$MARCH --with-abi=$MABI --enable-checking=none
 make -j$(nproc) all
 CODE=$?
 if [ $CODE -ne 0 ]; then
@@ -69,14 +73,14 @@ make install
 
 # Linux Headers Stage 1
 cd "$LINUX_SOURCE_DIR"
-make ARCH=riscv INSTALL_HDR_PATH="$PREFIX_DIR/tools/usr" headers_install
+make ARCH=riscv INSTALL_HDR_PATH="$SYSROOT/tools/usr" headers_install
 
 # MUSL Stage 1
-cd "$MUSL_SOURCE_DIR"
+cd "$LIBC_SOURCE_DIR"
 rm -rf ./build1
 mkdir ./build1
 cd ./build1
-./../configure --host=$TGT2 --build=$HOST --prefix="$PREFIX_DIR/tools/usr" --disable-wrapper --disable-shared CROSS_COMPILE=$TGT1- CFLAGS="-march=$MARCH -mabi=$MABI -O3 -pipe -fPIC"
+./../configure --host=$TGT2 --build=$HOST --prefix="$SYSROOT/tools/usr" --disable-wrapper --disable-shared CROSS_COMPILE=$TGT1- CFLAGS="-march=$MARCH -mabi=$MABI -O3 -pipe -fPIC"
 make -j$(nproc) all
 CODE=$?
 if [ $CODE -ne 0 ]; then
@@ -90,7 +94,7 @@ cd "$BINUTILS_SOURCE_DIR"
 rm -rf ./build2
 mkdir ./build2
 cd ./build2
-./../configure --target=$TGT2 --host=$HOST --prefix="$PREFIX_DIR/tools/usr" --with-sysroot="$PREFIX_DIR/tools" --disable-nls --disable-werror
+./../configure --target=$TGT2 --host=$HOST --prefix="$SYSROOT/tools/usr" --with-sysroot="$SYSROOT/tools" --disable-nls --disable-werror
 make -j$(nproc) all
 CODE=$?
 if [ $CODE -ne 0 ]; then
@@ -104,7 +108,7 @@ cd "$GCC_SOURCE_DIR"
 rm -rf ./build2
 mkdir ./build2
 cd ./build2
-./../configure --target=$TGT2 --host=$HOST --prefix="$PREFIX_DIR/tools/usr" --with-sysroot="$PREFIX_DIR/tools" --disable-multilib --with-arch=$MARCH --with-abi=$MABI --enable-languages=c,c++ --disable-libstdcxx
+./../configure --target=$TGT2 --host=$HOST --prefix="$SYSROOT/tools/usr" --with-sysroot="$SYSROOT/tools" --with-mpc="$MPC_SOURCE_DIR" --with-mpfr="$MPFR_SOURCE_DIR" --with-gmp="$GMP_SOURCE_DIR" --disable-multilib --with-arch=$MARCH --with-abi=$MABI --enable-languages=c,c++ --disable-libstdcxx --enable-checking=none
 make -j$(nproc) all
 CODE=$?
 if [ $CODE -ne 0 ]; then
@@ -115,14 +119,14 @@ make install
 
 # Linux Headers Stage 2
 cd "$LINUX_SOURCE_DIR"
-make ARCH=riscv INSTALL_HDR_PATH="$PREFIX_DIR/usr" headers_install
+make ARCH=riscv INSTALL_HDR_PATH="$SYSROOT/usr" headers_install
 
 # MUSL Stage 2
-cd "$MUSL_SOURCE_DIR"
+cd "$LIBC_SOURCE_DIR"
 rm -rf ./build2
 mkdir ./build2
 cd ./build2
-./../configure --host=$TGT3 --build=$HOST --prefix="$PREFIX_DIR/usr" --disable-wrapper CROSS_COMPILE=$TGT2- CFLAGS="-march=$MARCH -mabi=$MABI -O3 -pipe -fPIC"
+./../configure --host=$TGT3 --build=$HOST --prefix="$SYSROOT/usr" --disable-wrapper CROSS_COMPILE=$TGT2- CFLAGS="-march=$MARCH -mabi=$MABI -O3 -pipe -fpic -g"
 make -j$(nproc) all
 CODE=$?
 if [ $CODE -ne 0 ]; then
@@ -136,7 +140,7 @@ cd "$BINUTILS_SOURCE_DIR"
 rm -rf ./build3
 mkdir ./build3
 cd ./build3
-./../configure --target=$TGT3 --host=$HOST --prefix="$PREFIX_DIR/usr" --with-sysroot="$PREFIX_DIR" --disable-werror
+./../configure --target=$TGT3 --host=$HOST --prefix="$SYSROOT/usr" --with-sysroot="$SYSROOT" --disable-werror
 make -j$(nproc) all
 CODE=$?
 if [ $CODE -ne 0 ]; then
@@ -150,7 +154,7 @@ cd "$GCC_SOURCE_DIR"
 rm -rf ./build3
 mkdir ./build3
 cd ./build3
-./../configure --target=$TGT3 --host=$HOST --prefix="$PREFIX_DIR/usr" --with-sysroot="$PREFIX_DIR" --disable-multilib --with-arch=$MARCH --with-abi=$MABI --enable-languages=c,c++ --disable-libsanitizer
+./../configure --target=$TGT3 --host=$HOST --prefix="$SYSROOT/usr" --with-sysroot="$SYSROOT" --with-mpc="$MPC_SOURCE_DIR" --with-mpfr="$MPFR_SOURCE_DIR" --with-gmp="$GMP_SOURCE_DIR" --disable-multilib --with-arch=$MARCH --with-abi=$MABI --enable-languages=c,c++ --enable-default-pie --enable-checking=none
 make -j$(nproc) all
 CODE=$?
 if [ $CODE -ne 0 ]; then
@@ -159,6 +163,6 @@ if [ $CODE -ne 0 ]; then
 fi
 make install
 
-rm -rf "$PREFIX_DIR/tools"
+rm -rf "$SYSROOT/tools"
 
 exit 0
